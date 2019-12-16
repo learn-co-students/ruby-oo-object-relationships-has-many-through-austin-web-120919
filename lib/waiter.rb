58c19a0f2ece3bfd5d7c86103102d3a94e2b1e21
Chance Mims <chance@Chances-MacBook-Air.local>
2019-12-16 16:36:13 -0600

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
 
+    attr_accessor :meal
+
+    @@all = []
+
+
+    def initialize(name, years_of_experience)
+        @name = name
+        @years_of_experience = years_of_experience
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip = 0)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def meals
+        Meal.all.find_all{|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+       # binding.pry
+        meals.max{|a, b| a.tip <=> b.tip}.customer
+    end
+
 end
